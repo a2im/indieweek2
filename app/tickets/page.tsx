02f5ebf6b86986f98578e48743cb4b1e26ff396d
wobble-house <ross@wobble.house>
2022-12-15 13:35:36 -0500

    updating ticket page

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,5 +1,5 @@
-import { SocialFollowWhite } from '../SocialFollow'
 import Footer from '../footer'
+import MyTickets from './tickets'
 
 export default function Tickets() {
   return (
@@ -10,20 +10,7 @@ export default function Tickets() {
 Tickets
   </h2>
   </div>
-          <div className="mx-auto mb-8 py-20"> 
-            <div className='flex max-w-4xl mb-10 mx-auto gap-10 justify-center'> 
-              <div className='mx-auto pb-10 p-4'>
-                <h3 className="mb-6 text-center">
-                  COMING SOON 
-                </h3>
-                <h4 className="text-center">Follow us on social media for the latest updates and subscribe
-to the Libera Awards mailing list to hear first about ticket availability!</h4>
-              <div className="max-w-lg pt-20 mx-auto">
-              <SocialFollowWhite/>
-              </div>
-              </div>
-              </div>
-          </div>
+<MyTickets/>
           <Footer/>
     </div>
   )

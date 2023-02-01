704c53705439f209a9e0c2b5c11fa753b5b87af5
wobble-house <ross@wobble.house>
2023-02-01 15:56:31 -0500

    fixing build errors from head component

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,19 +1,16 @@
 import GetFAQ from './faq-categories'
 import { JumpTo } from './faq-categories'
 import Footer from '../footer'
-import Head from 'next/head'
 
 export default function FAQ() {
   return (
     <div className="bggradient pt-8">
-      <Head>
       <title>Indie Week - Program</title>
       <meta
           name="description"
           content="Founded by independents, for independents, A2IM Indie Week is a four-day conference for the independent music community in the United States."
           key="desc"
         />
-      </Head>
       <div className="pb-96 md:pb-64">
         <JumpTo/>
         <GetFAQ Category='GENERAL'/>
